79375b1be323b30a322458c4073a204a95382ada
lecho <leszekwach@gmail.com>
2013-02-28 21:28:16 +0100

    Add LoaderCallbacks to PlaceDetailsActivity.

--- src/lecho/app/campus/PlaceDetailsActivity.java
+++ src/lecho/app/campus/PlaceDetailsActivity.java
@@ -1,7 +1,37 @@
 package lecho.app.campus;
 
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
+
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
-public class PlaceDetailsActivity extends SherlockFragmentActivity {
+public class PlaceDetailsActivity extends SherlockFragmentActivity implements LoaderCallbacks<Cursor> {
+    private static final int PLACE_LOADER_ID = 1;
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (id == PLACE_LOADER_ID) {
+            // TODO
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+        if (loader.getId() == PLACE_LOADER_ID) {
+            // TODO
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        if (loader.getId() == PLACE_LOADER_ID) {
+            // TODO
+        }
+
+    }
 
 }
