ddbe1081e9ad8f951677fde1c64dd3f1962618e4
lecho <leszekwach@gmail.com>
2013-03-01 20:08:53 +0100

    Add docs and simple methods.

--- src/lecho/app/campus/MapActivity.java
+++ src/lecho/app/campus/MapActivity.java
@@ -7,14 +7,38 @@ import android.support.v4.content.Loader;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
+/**
+ * Main map activity. Shows area of whole campus.
+ * 
+ * @author lecho
+ * 
+ */
 public class MapActivity extends SherlockFragmentActivity implements LoaderCallbacks<Cursor> {
     private static final int PLACES_LOADER_ID = 1;
+    private static final String TAG = MapActivity.class.getSimpleName();
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_place_details);
+        setContentView(R.layout.activity_map);
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the
+        // map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                // TODO setup google map
+            }
+        }
     }
 
     @Override
@@ -26,7 +50,7 @@ public class MapActivity extends SherlockFragmentActivity implements LoaderCallb
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         if (id == PLACES_LOADER_ID) {
-            // TODO
+            // TODO create cursor loader and apply filters
         }
         return null;
     }
@@ -34,7 +58,7 @@ public class MapActivity extends SherlockFragmentActivity implements LoaderCallb
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
         if (loader.getId() == PLACES_LOADER_ID) {
-            // TODO
+            // TODO setup map(markers etc)
         }
 
     }
@@ -42,7 +66,7 @@ public class MapActivity extends SherlockFragmentActivity implements LoaderCallb
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         if (loader.getId() == PLACES_LOADER_ID) {
-            // TODO
+            mMap.clear();
         }
 
     }
--- src/lecho/app/campus/PlaceDetailsActivity.java
+++ src/lecho/app/campus/PlaceDetailsActivity.java
@@ -7,8 +7,21 @@ import android.support.v4.content.Loader;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
+/**
+ * Displays single place details. On xlarge screens displays detailed map also.
+ * 
+ * @author lecho
+ * 
+ */
 public class PlaceDetailsActivity extends SherlockFragmentActivity implements LoaderCallbacks<Cursor> {
     private static final int PLACE_LOADER_ID = 1;
+    public static final String PLACE_ID = "lecho.app.campus:PLACE_ID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
