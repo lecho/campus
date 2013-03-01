4023ae198b65037114d5db3cdcb26c02738145ac
lecho <leszekwach@gmail.com>
2013-03-01 20:08:40 +0100

    Update Config.java.

--- src/lecho/app/campus/utils/Config.java
+++ src/lecho/app/campus/utils/Config.java
@@ -5,5 +5,7 @@ public final class Config {
     public static final String PHOTOS_FOLDER = "photos";
     public static final boolean LOGV = true;
     public static final boolean LOGD = true;
+    public static final String APP_PREFS_NAME = "campus_prefs";
+    public static final String APP_FIRST_START = "lecho.app.campus:FIRST_START";
 
 }
