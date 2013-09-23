801985a4c1177c7cb8d823ea26f317acda175b39
Lecho <leszekwach@gmail.com>
2013-09-23 23:14:18 +0200

    Add up button to PlaceDetailsActivity.java.

--- src/lecho/app/campus/activity/PlaceDetailsActivity.java
+++ src/lecho/app/campus/activity/PlaceDetailsActivity.java
@@ -5,6 +5,8 @@ import lecho.app.campus.utils.Config;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class PlaceDetailsActivity extends SherlockFragmentActivity {
 
@@ -15,6 +17,22 @@ public class PlaceDetailsActivity extends SherlockFragmentActivity {
 		Long placeId = getIntent().getLongExtra(Config.EXTRA_PLACE_ID, Long.MIN_VALUE);
 		PlaceDetailsFragment fragment = PlaceDetailsFragment.newInstance(placeId);
 		getSupportFragmentManager().beginTransaction().add(android.R.id.content, fragment).commit();
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int itemId = item.getItemId();
+		if (android.R.id.home == itemId) {
+			this.finish();
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
