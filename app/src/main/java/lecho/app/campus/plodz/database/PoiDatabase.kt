f7faafb6b2cc8ec374557e0143833eda19cb3530
Leszek Wach <leszekwach@gmail.com>
2018-05-07 20:56:26 +0200

    Update dependencies for Room and Kotlin

--- app/src/main/java/lecho/app/campus/plodz/database/PoiDatabase.kt
+++ app/src/main/java/lecho/app/campus/plodz/database/PoiDatabase.kt
@@ -4,7 +4,7 @@ import android.arch.persistence.room.RoomDatabase
 import android.arch.persistence.room.Database
 
 
-@Database(entities = [Poi::class, Faculty::class, Unit::class], version = 1)
+@Database(entities = [Poi::class, Faculty::class, Unit::class], version = 1, exportSchema = false)
 abstract class PoiDatabase: RoomDatabase() {
 
 }
