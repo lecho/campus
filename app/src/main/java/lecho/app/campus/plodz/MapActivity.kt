d2f76d7b731ae6d995ee8fc23e9d69917eebfcbc
Leszek Wach <leszekwach@gmail.com>
2018-10-13 15:25:33 +0200

    Use default zoom and LatLng

--- app/src/main/java/lecho/app/campus/plodz/MapActivity.kt
+++ app/src/main/java/lecho/app/campus/plodz/MapActivity.kt
@@ -10,10 +10,9 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_map.*
-import kotlinx.android.synthetic.main.poi_info_view.*
 import lecho.app.campus.plodz.repository.PoiRepository
 import lecho.app.campus.plodz.viewmodel.AllPois
 import lecho.app.campus.plodz.viewmodel.AllPoisViewModel
@@ -24,6 +23,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     companion object {
         @JvmField
         val TAG: String = MapActivity::class.java.simpleName
+        val DEFAULT_LATlNG = LatLng(51.7505298, 19.4551516)
+        const val DEFAULT_ZOOM = 14.8F
     }
 
     private lateinit var mMap: GoogleMap
@@ -34,6 +35,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_map)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        @Suppress("CAST_NEVER_SUCCEEDS")
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
@@ -63,10 +65,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             mMap.moveCamera(CameraUpdateFactory.newLatLng(position))
         })
 
-        // Add a marker in Sydney and move the camera
-//        val sydney = LatLng(-34.0, 151.0)
-        //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_LATlNG, DEFAULT_ZOOM))
     }
 
     private fun changeMapStyle(styleJson: Int) {
