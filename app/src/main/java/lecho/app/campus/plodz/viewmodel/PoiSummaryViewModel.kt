7f83c10399142d1c84911f91ae6031420295359a
Leszek Wach <leszekwach@gmail.com>
2018-10-18 21:01:36 +0200

    Remove unnecessary class

--- app/src/main/java/lecho/app/campus/plodz/viewmodel/PoiSummaryViewModel.kt
+++
@@ -1,6 +0,0 @@
-package lecho.app.campus.plodz.viewmodel
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-
-class PoiSummaryViewModel(var baseInfo: LiveData<PoiBaseInfo>) : ViewModel()
